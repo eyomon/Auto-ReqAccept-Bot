9fd82cd0e010eefee8efb901c85e1d3d6040dbb3
file21 <138193572+file21@users.noreply.github.com>
2023-11-19 13:18:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,7 +28,7 @@ async def start_handler(c, m):
     if not await Data.find_one({'id': user_id}): await Data.insert_one({'id': user_id})
     button = [[        
         InlineKeyboardButton('Updates', url='https://t.me/T4TSA'),
-        InlineKeyboardButton('Support', url='https://t.me/IrisChat0')
+        InlineKeyboardButton('Support', url='https://t.me/TheeLinguine')
     ]]
     return await m.reply_text(text=START_TEXT.format(m.from_user.mention), disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(button))
           
